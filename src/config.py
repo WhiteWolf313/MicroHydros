892acf9b5efbf235f673f23674f57060f1adf34b
WhiteWolf313 <dragonslayer.yugi94@gmail.com>
2026-09-02 10:41:29 +0200

    config & cloud

---
+++ src/config.py
@@ -0,0 +1,12 @@
+WIFI_SSID = "Ditt_WiFi"
+WIFI_PASS = "Ditt_Lösenord"
+
+AIO_SERVER = "io.adafruit.com"
+AIO_PORT = 1883
+AIO_USER = "WhiteWolf313"
+AIO_KEY = "aio_VgRF82IwXp6XInJ4I3qTgnOvNxZw"
+
+
+FEED_VATTEN = f"{AIO_USER}/f/vatten-temp"
+FEED_LUFT_INNE = f"{AIO_USER}/f/temp-inne"
+FEED_FUKT_INNE = f"{AIO_USER}/f/fukt-inne"
